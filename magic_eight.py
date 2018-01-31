003f1908097fea0eda570d5d93483a99ec76e06a
Andrew Yuan <yuandrew@umich.edu>
2018-01-31 17:07:15 -0500

    Update magic_eight.py

--- magic_eight.py
+++ magic_eight.py
@@ -2,7 +2,6 @@ def ask_question():
     answer = input('What is your question? ')
 
 
-<<<<<<< HEAD
 list1 = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
  "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes",
  "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now",
@@ -11,13 +10,10 @@ list1 = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definite
 
 
 
-
-print (list1[randrange(len(list1))])
-=======
 answer = ask_question()
 while(answer != quit):
     if answer[-1] == ‘?’:
-
+        print (list1[randrange(len(list1))])
     else:
         print(‘I\’m sorry, I can only answer questions.’)
->>>>>>> check_question
+
