f39c4893ab9bc6115a82a36bfd29312d768af2ee
Andrew Yuan <yuandrew@umich.edu>
2018-01-31 16:57:01 -0500

    third commit

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,10 @@
 def ask_question():
     answer = input('What is your question? ')
+
+
+answer = ask_question()
+while(answer != quit):
+    if answer[-1] == ‘?’:
+
+    else:
+        print(‘I\’m sorry, I can only answer questions.’)
